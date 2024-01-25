3e8aa99545130d4ce1a76b91cae1f0fde24274f9
kishor96k <chavankishor813@gmail.com>
2024-01-25 11:11:47 +0530

    added the UI changes for the angular crud with localstorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OneComponent } from './one_practice/one/one.component';
 import { TwoComponent } from './two_practice/two/two.component';
+import { OneComponent } from './one_practice/one/one.component';
+
 
 const routes: Routes = [
   {
-    path: '',
-    component: TwoComponent
+    path: 'one',
+    component: OneComponent,
+    title:'inline editing'
+  },
+  {
+    path: 'two',
+    component: TwoComponent,
+    title: 'crud with local storage'
   }
 ];
 
