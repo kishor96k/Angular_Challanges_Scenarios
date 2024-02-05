f62cfd267adbc8e247c2f903f093438c7076e580
kishor96k <chavankishor813@gmail.com>
2024-02-05 10:35:17 +0530

    updated the appmodule file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
 import { OneComponent } from './one_practice/one/one.component';
 import { TwoComponent } from './two_practice/two/two.component';
+import { ThreeComponent } from './three_practice/three/three.component';
+import { FourComponent } from './four_practice/four/four.component';
+import { FiveComponent } from './five_practice/five/five.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OneComponent,
-    TwoComponent
+    TwoComponent,
+    ThreeComponent,
+    FourComponent,
+    FiveComponent
   ],
   imports: [
     BrowserModule,
