501fa0d5c514ddc00d08a4406fee9b1a9ac5f627
kishor96k <chavankishor813@gmail.com>
2024-02-05 10:36:54 +0530

    addded the route for the three

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TwoComponent } from './two_practice/two/two.component';
 import { OneComponent } from './one_practice/one/one.component';
+import { ThreeComponent } from './three_practice/three/three.component';
 
 
 const routes: Routes = [
@@ -14,7 +15,22 @@ const routes: Routes = [
     path: 'two',
     component: TwoComponent,
     title: 'crud with local storage'
-  }
+  },
+  {
+    path: 'three',
+    component: ThreeComponent,
+    title: 'crud with local storage'
+  },
+  {
+    path: 'two',
+    component: TwoComponent,
+    title: 'crud with local storage'
+  },
+  {
+    path: 'two',
+    component: TwoComponent,
+    title: 'crud with local storage'
+  },
 ];
 
 @NgModule({
