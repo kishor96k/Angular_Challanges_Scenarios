8f534039481941663efb47732daf45e424a47ee0
kishor96k <chavankishor813@gmail.com>
2024-02-12 11:49:50 +0530

    form added for the local crud

--- src/app/two_practice/two/two.component.ts
+++ src/app/two_practice/two/two.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-two',
   templateUrl: './two.component.html',
   styleUrls: ['./two.component.css']
 })
-export class TwoComponent {
+export class TwoComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+    const localData = localStorage.getItem('ComplaintAppData');
+    if (localData != null) {
+      this.complaintArray = JSON.parse(localData);
+      console.log(this.complaintArray);      
+    }
+  }
+
+  complaintObj: any = {
+    id: 0,
+    complaintAgainst: '',
+    firstname: '',
+    lastname: '',
+    addressone: '',
+    city: '',
+    region: '',
+    pincode: 0,
+    phone: 0,
+    email: '',
+    complaint: '',
+  }
+
+  complaintArray: any[] = [];
+
+
+
+  onRegister() {
+    const localData = localStorage.getItem('ComplaintAppData');
+    if (localData == null) {
+      this.complaintObj.id = 1;
+      this.complaintArray.push(this.complaintObj);
+      localStorage.setItem('ComplaintAppData', JSON.stringify(this.complaintArray));
+    } else {
+      const parseData = JSON.parse(localData);
+      this.complaintObj.id = parseData.length + 1;
+      this.complaintArray.push(this.complaintObj);
+      localStorage.setItem('ComplaintAppData', JSON.stringify(this.complaintArray));
+    }
+  }
 }
