7b8b5055cd308df2604e4415a0fa57835831e7ab
kishor96k <chavankishor813@gmail.com>
2024-02-13 09:38:48 +0530

    added the second folder for the project

--- src/app/two_practice/two/two.component.ts
+++ src/app/two_practice/two/two.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'bootstrap';
 
 @Component({
   selector: 'app-two',
@@ -6,18 +8,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./two.component.css']
 })
 export class TwoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-
-    const localData = localStorage.getItem('ComplaintAppData');
-    if (localData != null) {
-      this.complaintArray = JSON.parse(localData);
-      console.log(this.complaintArray);      
-    }
-  }
 
   complaintObj: any = {
     id: 0,
@@ -31,23 +21,46 @@ export class TwoComponent implements OnInit {
     phone: 0,
     email: '',
     complaint: '',
-  }
+  };
 
   complaintArray: any[] = [];
 
+  constructor() { }
 
+  ngOnInit(): void {
+    this.loadLocalData();
+  }
 
-  onRegister() {
+  private loadLocalData(): void {
     const localData = localStorage.getItem('ComplaintAppData');
-    if (localData == null) {
-      this.complaintObj.id = 1;
-      this.complaintArray.push(this.complaintObj);
+    if (localData) {
+      this.complaintArray = JSON.parse(localData);
+      console.log(this.complaintArray);
+    }
+  }
+
+  onRegister(): void {
+    const localData = localStorage.getItem('ComplaintAppData');
+    const newData = localData ? JSON.parse(localData) : [];
+
+    this.complaintObj.id = newData.length + 1;
+    newData.push({ ...this.complaintObj });
+
+    localStorage.setItem('ComplaintAppData', JSON.stringify(newData));
+    this.loadLocalData(); // Refresh the data after adding a new complaint
+  }
+
+  onEdit(index: number): void {
+    this.complaintObj = { ...this.complaintArray[index] };
+    $('#editModal').modal('show');
+  }
+
+  onUpdate(): void {
+    const index = this.complaintArray.findIndex(item => item.id === this.complaintObj.id);
+    if (index !== -1) {
+      this.complaintArray[index] = { ...this.complaintObj };
       localStorage.setItem('ComplaintAppData', JSON.stringify(this.complaintArray));
-    } else {
-      const parseData = JSON.parse(localData);
-      this.complaintObj.id = parseData.length + 1;
-      this.complaintArray.push(this.complaintObj);
-      localStorage.setItem('ComplaintAppData', JSON.stringify(this.complaintArray));
+      $('#editModal').modal('hide');
     }
   }
 }
