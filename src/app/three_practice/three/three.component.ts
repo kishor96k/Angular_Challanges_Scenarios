a024048e9a23ad3844a31e9d3b17510fd5a84aae
kishor96k <chavankishor813@gmail.com>
2024-02-13 12:47:02 +0530

    changes added for the age , licence , date in the template driven form

--- src/app/three_practice/three/three.component.ts
+++ src/app/three_practice/three/three.component.ts
@@ -31,7 +31,35 @@ export class ThreeComponent implements OnInit {
   }
 
   fillName: any;
+  showLicence:boolean=false;
   createFullName() {
     this.fillName = this.templateForm.firstname + ' ' + this.templateForm.middlename + ' ' + this.templateForm.lastname;
+  }
+  getCurrentDate(): string {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    let month: string | number = currentDate.getMonth() + 1;
+    let day: string | number = currentDate.getDate();
+
+    // Add leading zero if month or day is a single digit
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    return `${year}-${month}-${day}`;
+  }
+
+  onDOBChange() {
+    const SelectedDate = new Date(this.templateForm.dob);
+    const selectedDateyear = SelectedDate.getFullYear();
+    const currentYear = new Date().getFullYear();
+    console.log(selectedDateyear);
+    console.log(currentYear);
+    this.templateForm.age = currentYear - selectedDateyear;
+    if(this.templateForm.age >=18){
+      this.showLicence=true
+    }
+
+
+
   }
 }
