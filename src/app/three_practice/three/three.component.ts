2afc00906a2e66bb11faecaffc122e5bfbd8d52e
kishor96k <chavankishor813@gmail.com>
2024-02-13 11:44:18 +0530

    object added for the template driven form

--- src/app/three_practice/three/three.component.ts
+++ src/app/three_practice/three/three.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-three',
   templateUrl: './three.component.html',
   styleUrls: ['./three.component.css']
 })
-export class ThreeComponent {
+export class ThreeComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
 
+  templateForm: any = {
+    firstname: '',
+    middlename: '',
+    lastname: '',
+    dob: '',
+    ahge: '',
+    licence: '',
+    education: '',
+    twelthper: '',
+    twelthpasssyr: '',
+    degree1styrper: '',
+    degree2ndyrper: '',
+    degree3rdyrper: '',
+    degreepassyr:'',
+    othereducation:'',
+    othereducationper:'',
+    othereducationyr:'',
+    startdate:'',
+    enddate:'',
+  }
+  
 }
