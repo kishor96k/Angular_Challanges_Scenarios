3e792925d0d7416b78601d2e32fa172e3b5ae8c0
kishor96k <chavankishor813@gmail.com>
2024-02-14 11:34:48 +0530

    chages done by fileupload functionality

--- src/app/four_practice/four/four.component.ts
+++ src/app/four_practice/four/four.component.ts
@@ -11,20 +11,25 @@ export class FourComponent implements OnInit {
   constructor(
     private http: HttpClient
   ) { }
+
   ngOnInit(): void {
   }
+
+  uploadedFilesNames: string[] = [];
+  fileUrl: any = 'https://storeapi.gerasim.in/api/Customer/Upload';
   onFileUpload(event: any) {
     const file = event.currentTarget.files[0]
-    // console.log(file);
-    const formObj = new FormData();
-    formObj.append('file', file);
-
-    this.http.post('https://storeapi.gerasim.in/api/Customer/Upload', formObj).subscribe((res: any) => {
-      console.log(res);
-      
-    })
-
-
+    console.log("addedfile", file);
+    if (file.type == 'image/jpeg' || file.type == 'image/png') {
+      const formObj = new FormData();
+      formObj.append('file', file);
+      this.http.post('https://storeapi.gerasim.in/api/Customer/Upload', formObj).subscribe((res: any) => {
+        console.log(res);
+        this.uploadedFilesNames.push(res);
+      })
+    } else {
+      alert("only png/jpeg files are allowed to upload");
+    }
   }
 
 
