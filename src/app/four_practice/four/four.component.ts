1708fba0ef41cef0987ad97b5b472f22dd7ddda9
kishor96k <chavankishor813@gmail.com>
2024-02-14 09:07:28 +0530

    added the route for the four proejct

--- src/app/four_practice/four/four.component.ts
+++ src/app/four_practice/four/four.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-four',
   templateUrl: './four.component.html',
   styleUrls: ['./four.component.css']
 })
-export class FourComponent {
+export class FourComponent implements OnInit {
+  
+  constructor() { }
+  ngOnInit(): void {
+  }
+  
 
 }
