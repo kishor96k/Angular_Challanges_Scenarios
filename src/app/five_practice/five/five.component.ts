580cf817c950485e08ee202ae1ddd5b8a7349d00
kishor96k <chavankishor813@gmail.com>
2024-02-15 10:37:18 +0530

    dept list added , form design and ts changes

--- src/app/five_practice/five/five.component.ts
+++ src/app/five_practice/five/five.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Import HttpClient
 
 @Component({
   selector: 'app-five',
   templateUrl: './five.component.html',
   styleUrls: ['./five.component.css']
 })
-export class FiveComponent {
+export class FiveComponent implements OnInit {
+  constructor(
+    private http: HttpClient
+  ) { }
 
+  depturl: any = 'http://localhost:3000/departments';
+
+  ngOnInit(): void {
+    this.loadDepartmentData();
+  }
+  departmantList: any;
+  loadDepartmentData() {
+    this.http.get(this.depturl).subscribe((res: any) => {
+      this.departmantList = res;
+    });
+  }
 }
