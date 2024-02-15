8ce88bafff7c0c938d2160f376b255363cbd66d9
kishor96k <chavankishor813@gmail.com>
2024-02-15 11:06:24 +0530

    final changes for the crud of users list

--- src/app/five_practice/five/five.component.ts
+++ src/app/five_practice/five/five.component.ts
@@ -22,15 +22,33 @@ export class FiveComponent implements OnInit {
   departmantList: any;
   usersList: any;
   isView: boolean = true;
+  usersObj: any = {
+    "id": 0,
+    "deptId": '',
+    "firstname": "",
+    "lastname": "",
+    "email": "",
+    "contact": 0
+  }
 
   loadDepartmentData() {
     this.http.get(this.depturl).subscribe((res: any) => {
       this.departmantList = res;
     });
   }
+
   loadusersList() {
     this.http.get(this.userurl).subscribe((res: any) => {
       this.usersList = res;
     })
+  }
+
+  postUsersList() {
+    this.http.post(this.userurl, this.usersObj).subscribe((res: any) => {
+      this.loadusersList(); // 
+      alert('data added');
+      this.isView = false;
+      console.log(res);
+    });
   }
 }
