b0eb8dd2ef8b116364225c1ece9fff30dc2e5556
kishor96k <chavankishor813@gmail.com>
2024-02-16 09:58:05 +0530

    routing and table list added for the userslist

--- src/app/six_practice/six/six.component.ts
+++ src/app/six_practice/six/six.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-six',
   templateUrl: './six.component.html',
   styleUrls: ['./six.component.css']
 })
-export class SixComponent {
+export class SixComponent implements OnInit {
+
+  usersList: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res: any) => {
+      this.usersList = res;
+    })
+  }
+
+
 
 }
