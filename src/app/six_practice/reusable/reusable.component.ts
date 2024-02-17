785ffe35e8132ae31612581d2c57e1294d1cb7c8
kishor96k <chavankishor813@gmail.com>
2024-02-17 12:45:36 +0530

    reusable compoent added

--- src/app/six_practice/reusable/reusable.component.ts
+++ src/app/six_practice/reusable/reusable.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-reusable',
   templateUrl: './reusable.component.html',
   styleUrls: ['./reusable.component.css']
 })
-export class ReusableComponent {
+export class ReusableComponent implements OnInit {
+
+  @Input() tableData: any;
+  @Input() coloumnKeys: any;
+  @Input() showActionButton:boolean=false;
+
+  @Output() onEdit = new EventEmitter<any>();
+  @Output() onDelete = new EventEmitter<any>();
+
+  constructor() {
+
+  }
+  ngOnInit() { }
+  onEditUser(item: any) {
+    this.onEdit.emit(item);
+  }
+  onDeleteUser(item: any) {
+    this.onDelete.emit(item);
+  }
 
 }
