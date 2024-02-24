988e30d8b39652299d8a73e9685ac13e78e2e752
kishor96k <chavankishor813@gmail.com>
2024-02-24 10:35:58 +0530

    added the implimentation for the login

--- src/app/seven_practice/seven/login/login.component.ts
+++ src/app/seven_practice/seven/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(
+    private Http: HttpClient
+  ) { }
+  ngOnInit() { }
+
+  Loginob: any = {
+    "EmailId": '',
+    "Password": ''
+  }
+  onLogin() {
+    this.Http.post('https://freeapi.miniprojectideas.com/api/user/login', this.Loginob).subscribe((res: any) => {
+      if (res.result) {
+        localStorage.setItem('loginToken', res.data.token)
+      }
+    })
+  }
+
 
 }
